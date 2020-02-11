8ea4c67f97523b9c6c8887f7f5387261eeabb9d0
Sam Levy <slevy239@gmail.com>
2020-02-11 14:23:33 -0500

    revert

--- models/index.js
+++ models/index.js
@@ -1,8 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-    Odds: require('./Odds'),
-=======
     Jobs: require("./Jobs"),
     Angel: require("./AngelJobs");
->>>>>>> parent of 2c59060... houston db
   };
